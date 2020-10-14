82269e9bf9a015f03f13b08716d9956c88a67824
ynjoe <yuan.joe12@gmail.com>
2020-10-13 19:48:43 -0700

    Initial commit

---
+++ src/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const http = require("http").Server(app);
+const port = 8000;
+const io = require('socket.io')(http);
+
+io.on("connection", (socket) => {
+  let game = require("chess.js").Chess();
+  socket.emit("position", game.fen());
+  socket.on("move", move => {
+    if (game.move(move) !== null) {
+      socket.emit("position", game.fen());
+    }
+  });
+});
+
+http.listen(port, function() {
+    console.log('listening on *: ' + port);
+});
