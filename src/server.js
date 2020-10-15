4571071257ec9c8793d9ae923087eef582f9d56f
ynjoe <yuan.joe12@gmail.com>
2020-10-14 23:58:32 -0700

    Import updates

--- src/server.js
+++ src/server.js
@@ -1,11 +1,12 @@
+const { Chess } = require("chess.js");
 const express = require("express");
 const app = express();
 const http = require("http").Server(app);
 const port = process.env.PORT || 8000;
 const io = require('socket.io')(http);
 
-io.on("connection", (socket) => {
-  let game = require("chess.js").Chess();
+io.on("connection", socket => {
+  let game = new Chess();
   socket.emit("position", game.fen());
   socket.on("move", move => {
     if (game.move(move) !== null) {
@@ -14,6 +15,6 @@ io.on("connection", (socket) => {
   });
 });
 
-http.listen(port, function() {
+http.listen(port, () => {
     console.log('listening on *: ' + port);
 });
